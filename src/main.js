348463df77d18fe2cc0fa57084eadc1b7d27ef2f
vagrant <vagrant@homestead>
2018-07-11 04:02:22 +0000

    完成编辑

--- src/main.js
+++ src/main.js
@@ -2,12 +2,27 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import router from './router'
+import './directives'
+import './components'
+// 引入 store/index.js 的默认值
+import store from './store'
+// 引入插件
+import VueSweetalert2 from './plugins/vue-sweetalert2'
+import Message from './plugins/message'
+
+// 使用插件
+Vue.use(VueSweetalert2)
+Vue.use(Message)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
+  // 注入 store
+  store,
   components: { App },
   template: '<App/>'
 })
---
+++ src/router/routes.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    path: '/auth/register',
+    name: 'Register',
+    component: () => import('@/views/auth/Register')
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/Home')
+  },
+  {
+    path: '*',
+    redirect: '/'
+  },
+  {
+    path: '/auth/login',
+    name: 'Login',
+    component: () => import('@/views/auth/Login')
+  },
+  // 编辑资料路由
+  {
+    path: '/users/1/edit',
+    component: () => import('@/views/users/Edit.vue'),
+    children: [
+      {
+        path: '',
+        name: 'EditProfile',
+        component: () => import('@/views/users/Profile.vue'),
+        meta: { auth: true }
+      }
+    ]
+  },
+]

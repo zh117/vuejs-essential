f047a1626bf01b37d4d0e99b163e96015561a7f8
vagrant <vagrant@homestead>
2018-07-11 09:37:11 +0000

    完成模拟文章增删改

--- src/router/routes.js
+++ src/router/routes.js
@@ -45,4 +45,24 @@ export default [
       }
     ]
   },
+  // Create
+  {
+    path: '/articles/create',
+    name: 'Create',
+    component: () => import('@/views/articles/Create'),
+    meta: { auth: true }
+  },
+  // Content
+  {
+    path: '/articles/:articleId/content',
+    name: 'Content',
+    component: () => import('@/views/articles/Content.vue')
+  },
+  // Edit
+  {
+    path: '/articles/:articleId/edit',
+    name: 'Edit',
+    component: () => import('@/views/articles/Create'),
+    meta: { auth: true }
+  },
 ]
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,17 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import ls from '../utils/localStorage'
 import router from '../router'
+// 引入 actions.js 的所有导出
+import * as moreActions from './actions'
 
 Vue.use(Vuex)
 
 const state = {
   user: ls.getItem('user'),
   // 添加 auth 来保存当前用户的登录状态
-  auth: ls.getItem('auth')
+  auth: ls.getItem('auth'),
+  // 所有文章状态
+  articles: ls.getItem('articles')
 }
 
 const mutations = {
@@ -20,6 +24,11 @@ const mutations = {
   UPDATE_AUTH(state, auth) {
     state.auth = auth
     ls.setItem('auth', auth)
+  },
+  // 更改所有文章的事件类型
+  UPDATE_ARTICLES(state, articles) {
+    state.articles = articles
+    ls.setItem('articles', articles)
   }
 }
 
@@ -43,11 +52,29 @@ const actions = {
     }
 
     commit('UPDATE_USER', user)
+  },
+  // 使用对象展开运算符混入 moreActions
+  ...moreActions
+}
+
+// 添加 getters
+const getters = {
+  getArticleById: (state) => (id) => {
+    let articles = state.articles
+
+    if (Array.isArray(articles)) {
+      articles = articles.filter(article => parseInt(id) === parseInt(article.articleId))
+      return articles.length ? articles[0] : null
+    } else {
+      return null
+    }
   }
 }
 
 const store = new Vuex.Store({
   state,
+  // 注册 getters
+  getters,
   mutations,
   actions
 })
